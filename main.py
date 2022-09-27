c3ff9607bdec3f9b2c55e2161dc72197b2b00191
Nicholas Short <nicholas.r.h.short@gmail.com>
2022-09-27 01:25:09 -0400

    Basic webserver up and running

---
+++ main.py
@@ -0,0 +1,43 @@
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import socket
+import os
+
+hostName = socket.gethostbyname(socket.gethostname())
+serverPort = 80
+
+class WebServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        if self.path == "/":
+            self.path = "index.html"
+
+        try:
+            
+            if self.path.split(".")[-1] == "html" or self.path.split(".")[-1] == "css":
+                f = open("./public" + self.path).read()
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(bytes(f, "utf-8"))
+            
+            elif self.path.split(".")[-1] == "jpg":
+                f = open("./public" + self.path, "rb").read()
+                self.send_response(200)
+                self.send_header("Content-type", "image/jpeg")
+                self.end_headers()
+                self.wfile.write(f)
+
+            else:
+                raise Exception()
+
+        except:
+            self.send_error(404, "File not found")
+
+def run(server_class=ThreadingHTTPServer, handler_class=WebServer):
+    server_address = (hostName, serverPort)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    print(hostName)
+    run()
+
